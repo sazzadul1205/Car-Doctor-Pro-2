c47f38cc98da0d9cfad32bf2c840f66c9af92f55
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2024-11-27 00:49:49 +0600

    Contacts Components compleat

--- src/Components/HomePager/Contacts.jsx
+++ src/Components/HomePager/Contacts.jsx
@@ -2,26 +2,30 @@ import React from "react";
 
 const Contacts = () => {
   return (
-    <div className="bg-black flex max-w-[1200px] mx-auto p-5">
-      <div>
-        <img src={"/calendar.png"} alt="" />
-        <div>
-          <p>We are open monday-friday</p>
-          <p>7:00 am - 9:00 pm</p>
+    <div className=" max-w-[1200px] mx-auto py-5 ">
+      <div className="bg-black flex px-10 py-28 justify-between rounded-2xl">
+        <div className="flex gap-4">
+          <img src={"/calendar.png"} alt="" className="w-10" />
+          <div>
+            <p className="text-sm">We are open monday-friday</p> 
+            <p className="text-xl">7:00 am - 9:00 pm</p>
+          </div>
         </div>
-      </div>
-      <div>
-        <img src={"/telephone.png"} alt="" />
-        <div>
-          <p>Have a question?</p>
-          <p>+2546 251 2658</p>
+
+        <div className="flex gap-4">
+          <img src={"/telephone.png"} alt="" className="w-10" />
+          <div>
+            <p className="text-sm">Have a question?</p>
+            <p className="text-xl">+2546 251 2658</p>
+          </div>
         </div>
-      </div>
-      <div>
-        <img src={"/location.png"} alt="" />
-        <div>
-          <p>Need a repair? our address</p>
-          <p>Liza Street, New York</p>
+
+        <div className="flex gap-4">
+          <img src={"/location.png"} alt="" className="w-10" />
+          <div>
+            <p className="text-sm">Need a repair? our address</p>
+            <p className="text-xl">Liza Street, New York</p>
+          </div>
         </div>
       </div>
     </div>
