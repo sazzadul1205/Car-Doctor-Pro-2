9afdbfcd4bed7a967447b182476c04b87f9f35fc
Sazzadul_Islam_Molla <Psazzadul@gmail.com>
2024-12-04 11:08:00 +0600

    Debug

--- src/middleware.js
+++ src/middleware.js
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 
 export const middleware = async (request) => {
   const { pathname } = request.nextUrl;
-  // const token = cookies(request).get("__Secure-next-auth.session-token"); //__Secure- for deployment on vercel
-  const token = cookies(request).get("next-auth.session-token"); //__Secure- for deployment on vercel
+  const token = cookies(request).get("__Secure-next-auth.session-token"); //__Secure- for deployment on vercel
+  // const token = cookies(request).get("next-auth.session-token"); //__Secure- for deployment on vercel
 
   // Exclude `/Services` but include its subpaths
   if (pathname === "/Services") {
